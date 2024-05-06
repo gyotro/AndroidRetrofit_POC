0e451218bd446c968f4fb29676bcf51ef5f7a77a
gyotro <giovanni.dintrono@gmail.com>
2024-05-06 21:36:33 +0200

    localdate time added

--- app/src/main/java/com/sap/testretrofit/repositories/FilterBuilder.kt
+++ app/src/main/java/com/sap/testretrofit/repositories/FilterBuilder.kt
@@ -1,10 +1,13 @@
 package com.sap.testretrofit.repositories
 
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
 
 class FilterBuilder(
-    val startDateTime: LocalDateTime,
-    val endDateTime: LocalDateTime = LocalDateTime.now(),
+    val startDateTime: LocalDateTime = LocalDateTime.of( LocalDate.now(), LocalTime.of(0,0,1) ),
+    val endDateTime: LocalDateTime = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS),
     val status: List<Status>,
     val name: String
 ) {
