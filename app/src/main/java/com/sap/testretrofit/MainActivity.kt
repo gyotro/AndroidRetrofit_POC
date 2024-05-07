2af183c1632ebf7f59976bfd0334f9edcccb4da7
Giovanni D'Introno <giovanni.dintrono@ext.esa.int>
2024-05-07 16:44:07 +0200

    modifying UI

--- app/src/main/java/com/sap/testretrofit/MainActivity.kt
+++ app/src/main/java/com/sap/testretrofit/MainActivity.kt
@@ -3,17 +3,23 @@ package com.sap.testretrofit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.sap.cpi_monitor.domain.resource.BaseModel
 import com.sap.testretrofit.presentation.screen.TokenViewModel
+import com.sap.testretrofit.presentation.ui.theme.LightGrey
+import com.sap.testretrofit.presentation.ui.theme.MediumGrey
 import com.sap.testretrofit.presentation.ui.theme.TestRetrofitTheme
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
@@ -35,26 +41,36 @@ class MainActivity : ComponentActivity(){
 @Composable
 fun TokenData(modifier: Modifier = Modifier, viewModel: TokenViewModel) {
     val interfaces = viewModel.cpiMoniFlow.collectAsState().value
-    when(interfaces) {
-        is BaseModel.Error -> {
-            Text(text = "Base Model Error: ${interfaces.error}")
-        }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(LightGrey)
+    ) {
+        when(interfaces) {
+            is BaseModel.Error -> {
+                Text(text = "Base Model Error: ${interfaces.error}")
+            }
 
-        BaseModel.Loading -> {
-            Text(text = "Base Model Loading")
-        }
+            BaseModel.Loading -> {
+                //    Text(text = "Base Model Loading")
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
 
-        is BaseModel.Success -> {
-            LazyColumn {
-                items(interfaces.data.d.results) { item ->
-                    Text(text = item.integrationFlowName)
+            is BaseModel.Success -> {
+                LazyColumn {
+                    items(interfaces.data.d.results) { item ->
+                        Text(text = item.integrationFlowName)
+                    }
                 }
             }
-        }
 
-        null -> {
-            Text(text = "Null case")
+            null -> {
+                Text(text = "Null case")
+            }
         }
     }
 }
+
 
