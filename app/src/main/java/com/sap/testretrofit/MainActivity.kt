6897b4def67fa728cf35f1c0fd22fa510a5ab6f1
Giovanni D'Introno <giovanni.dintrono@ext.esa.int>
2024-05-15 18:39:57 +0200

    managing date and time

--- app/src/main/java/com/sap/testretrofit/MainActivity.kt
+++ app/src/main/java/com/sap/testretrofit/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -61,17 +64,33 @@ fun MainScreen() {
             mutableStateOf(setValue("20"))
         }
 
-        var startDate by remember {
-            val yesterdayDT = LocalDate.now().minusDays(1).toString()  + " " + LocalTime.now().truncatedTo(ChronoUnit.SECONDS)
-            mutableStateOf(yesterdayDT)
-        }
-
         var nameFlow by remember {
             mutableStateOf("")
         }
 
+       /* var startDate by remember {
+            val yesterdayDT = LocalDate.now().minusDays(1).toString()
+            mutableStateOf(yesterdayDT)
+        }*/
+        var startDate by remember {
+            val yesterdayDT = LocalDate.now().minusDays(1).toString()
+            mutableStateOf(yesterdayDT)
+        }
+
+        var startDateState = rememberDatePickerState()
+        var startDateOpened by remember { mutableStateOf(false) }
+
+        var startTime by remember {
+            val yesterdayDT = LocalTime.now().truncatedTo(ChronoUnit.SECONDS).toString()
+            mutableStateOf(yesterdayDT)
+        }
+
         var endDate by remember {
-            val currentDT = LocalDate.now().toString() + " " + LocalTime.now().truncatedTo(ChronoUnit.SECONDS)
+            val currentDT = LocalDate.now().toString()
+            mutableStateOf(currentDT)
+        }
+        var endTime by remember {
+            val currentDT = LocalTime.now().truncatedTo(ChronoUnit.SECONDS).toString()
             mutableStateOf(currentDT)
         }
 
@@ -82,8 +101,8 @@ fun MainScreen() {
         LaunchedEffect(top, startDate, endDate, status) {
             if (top.toInt() > 0 && status.isNotEmpty()) {
                 val filterBuilder = FilterBuilder(
-                    startDateTime = FilterBuilder.getDateTimeFromString(startDate),
-                    endDateTime = FilterBuilder.getDateTimeFromString(endDate),
+                    startDateTime = FilterBuilder.getDateTimeFromString("$startDate $startTime"),
+                    endDateTime = FilterBuilder.getDateTimeFromString("$endDate $endTime"),
                     nameFlow = nameFlow,
                     status = status
                 )
@@ -101,50 +120,228 @@ fun MainScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 16.dp, vertical = 64.dp),
+                .padding(horizontal = 10.dp, vertical = 40.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
+/*            Text(
                 text = "Cloud Integration Monitoring App.",
                 color = Color.DarkGray,
                 fontSize = 25.sp,
                 fontWeight = FontWeight.Bold
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Box(
+            )*/
+      //      Spacer(modifier = Modifier.height(16.dp))
+/*            Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(55.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(MaterialTheme.colorScheme.secondary),
+                    .clip(RoundedCornerShape(8.dp)),
+//                    .background(MaterialTheme.colorScheme.secondary),
                 contentAlignment = Alignment.Center
-            ) {
+            ) {*/
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)) {
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth(fraction = 0.35F)
+                            .clip(RoundedCornerShape(8.dp)),
+                        value = top,
+                        label = {
+                            Text("Max Selection")
+                        },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        onValueChange = {
+                            top = setValue( it )
+                        },
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Gray,
+                            unfocusedContainerColor = Color.White,
+                            disabledContainerColor = Color.LightGray,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            errorIndicatorColor = Color.Transparent,
+                            focusedTextColor = Color.Black,
+                            unfocusedTextColor = Color.LightGray
+                        ),
+                        placeholder = {
+                            Text("Max Selection")
+                        }
+                    )
+                    Spacer(modifier = Modifier.fillMaxWidth(fraction = 0.1f))
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth(fraction = 1F)
+                            .clip(RoundedCornerShape(8.dp)),
+                        value = nameFlow,
+                        label = {
+                            Text("Iflow name")
+                        },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        onValueChange = {
+                            nameFlow = it
+                        },
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Gray,
+                            unfocusedContainerColor = Color.White,
+                            disabledContainerColor = Color.LightGray,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            errorIndicatorColor = Color.Transparent,
+                            focusedTextColor = Color.Black,
+                            unfocusedTextColor = Color.LightGray
+                        ),
+                        placeholder = {
+                            Text("Iflow name")
+                        }
+                    )
+ //               }
+
+            }
+     //       Spacer(modifier = Modifier.fillMaxWidth().height(7.dp))
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)) {
                 TextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = top,
+                    modifier = Modifier
+                        .fillMaxWidth(fraction = 0.5F)
+                        .clip(RoundedCornerShape(8.dp)),
+                    value = startDate,
+                    readOnly = true,
                     label = {
-                        Text("Max Selection")
+                        Text("Start Date")
                     },
+                    trailingIcon = { IconButton(
+                        onClick = {  startDateOpened = true  } // show de dialog
+                    ) {
+                        Icon(imageVector = Icons.Default.DateRange, contentDescription = "Calendar")
+                    } },
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     onValueChange = {
-                        top = setValue( it )
+                        startDate = it
                     },
                     colors = TextFieldDefaults.colors(
-                        focusedContainerColor = Color.Transparent,
-                        unfocusedContainerColor = Color.Transparent,
-                        disabledContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Gray,
+                        unfocusedContainerColor = Color.White,
+                        disabledContainerColor = Color.LightGray,
                         focusedIndicatorColor = Color.Transparent,
                         unfocusedIndicatorColor = Color.Transparent,
                         disabledIndicatorColor = Color.Transparent,
                         errorIndicatorColor = Color.Transparent,
-                        focusedTextColor = Color.White,
+                        focusedTextColor = Color.Black,
                         unfocusedTextColor = Color.LightGray
                     ),
                     placeholder = {
-                        Text("Max Selection")
+                        Text("StartDate")
+                    }
+                )
+                /*IconButton(
+                    onClick = {  startDateOpened = true  } // show de dialog
+                ) {
+                    Icon(imageVector = Icons.Default.DateRange, contentDescription = "Calendar")
+                }*/
+                if(startDateOpened) {
+                    DatePickerDialog(onDismissRequest = { startDateOpened = false },
+                        confirmButton = { 
+                            Button(onClick = { startDateOpened = false }) {
+                                Text(text = "Confirm")
+                            }
+                        }) {
+                        DatePicker(state = startDateState)
+                    }
+                }
+                Spacer(modifier = Modifier.fillMaxWidth(fraction = 0.1f))
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth(fraction = 1F)
+                        .clip(RoundedCornerShape(8.dp)),
+                    value = startTime,
+                    label = {
+                        Text("Start Time")
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    onValueChange = {
+                        startTime = it
+                    },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Gray,
+                        unfocusedContainerColor = Color.White,
+                        disabledContainerColor = Color.LightGray,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        errorIndicatorColor = Color.Transparent,
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.LightGray
+                    ),
+                    placeholder = {
+                        Text("Start Time")
+                    }
+                )
+
+            }
+     //       Spacer(modifier = Modifier.fillMaxWidth().height(7.dp))
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)) {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth(fraction = 0.5F)
+                        .clip(RoundedCornerShape(8.dp)),
+                    value = endDate,
+                    label = {
+                        Text("End Date")
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    onValueChange = {
+                        endDate = it
+                    },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Gray,
+                        unfocusedContainerColor = Color.White,
+                        disabledContainerColor = Color.LightGray,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        errorIndicatorColor = Color.Transparent,
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.LightGray
+                    ),
+                    placeholder = {
+                        Text("End Date")
+                    }
+                )
+                Spacer(modifier = Modifier.fillMaxWidth(fraction = 0.1f))
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth(fraction = 1F)
+                        .clip(RoundedCornerShape(8.dp)),
+                    value = endTime,
+                    label = {
+                        Text("End Time")
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    onValueChange = {
+                        endTime = it
+                    },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Gray,
+                        unfocusedContainerColor = Color.White,
+                        disabledContainerColor = Color.LightGray,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        errorIndicatorColor = Color.Transparent,
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.LightGray
+                    ),
+                    placeholder = {
+                        Text("End Time")
                     }
                 )
             }
+
             /*       Box(
             modifier = Modifier
                 .fillMaxSize()
